e588efe1da2fbfd153babab81809a0222b54dc2f
tkatchik <tkatchik@gmail.com>
2019-08-03 12:48:50 +0200

    .

--- lesson20/src/modules/togglePopUp.js
+++ lesson20/src/modules/togglePopUp.js
@@ -1,42 +1,39 @@
 const togglePopUp = () => {
-    const popUp = document.querySelector('.popup'),
+    let popUp = document.querySelector('.popup'),
         popUpContent = document.querySelector('.popup-content'),
-        popUpBtn = document.querySelectorAll('.popup-btn');
+        popUpBtn = document.querySelectorAll('.popup-btn'),
+        count = 0,
+        moveAnimation;
 
+    const popUpAnimate = () => {
+        moveAnimation = requestAnimationFrame(popUpAnimate);
 
-    popUpBtn.forEach((elem) => {
-        if (window.innerWidth >= 500) {
-            elem.addEventListener('click', () => {
-                popUp.style.display = 'block';
-
-                let moveAnimate = Date.now(),
-
-                    animateTimer = setInterval(function() {
-                        let timeSet = Date.now() - moveAnimate;
-
-                        if (timeSet >= 2000) {
-                            clearInterval(animateTimer);
-                            return;
-                        }
-                        moving(timeSet);
-
-                        function moving(timeSet) {
-                            popUpContent.style.top = timeSet / 35 + 'px';
-                        }
-                    }, 20);
-            });
-        } else if (window.innerWidth < 500) {
-            elem.addEventListener('click', () => {
-                popUp.style.display = 'block';
-            });
+        if (count < 50) {
+            count += 1;
+            popUpContent.style = `transform: translateY(-${count}px)`; // counter, moving
+        } else {
+            cancelAnimationFrame(moveAnimation);
         }
+    };
+    const getAnim = () => {
+        if (screen.width > 760) {
+            popUp.style = `display: block`;
+            popUpAnimate();
+        } else {
+            popUp.style = `display: block`;
+        }
+    };
+    popUpBtn.forEach((item) => {
+        item.addEventListener('click', () => {
+            getAnim();
+        });
     });
-
     popUp.addEventListener('click', (event) => {
         let target = event.target;
 
         if (target.classList.contains('popup-close')) {
             popUp.style.display = 'none';
+            count = 0;
         } else {
             target = target.closest('.popup-content');
 
